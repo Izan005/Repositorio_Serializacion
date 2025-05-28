f778d854fb242c19117025e2162738ea18147c17
Izan005 <izanlopez1010@gmail.com>
2025-05-28 19:49:03 +0200

    no message

--- src/main/java/org/example/JSON_Actividades/Practica1/JsonVideojuegos.java
+++ src/main/java/org/example/JSON_Actividades/Practica1/JsonVideojuegos.java
@@ -14,9 +14,21 @@ public class JsonVideojuegos {
     public static void main(String[] args) {
 
         //Guardar videojuegos en una lista
+        //Videojuegos de play
         List<Videojuego> listaVideojuegos = new ArrayList<>(Arrays.asList(new Videojuego("Metal Gear Solid V: The Phantom Pain", "PS3", 60, true, Arrays.asList("Acción", "Espionaje Táctico"))));
         listaVideojuegos.add(new Videojuego("Devil May Cry 3", "PS2", 30, false, Arrays.asList("Acción")));
         listaVideojuegos.add(new Videojuego("Sekiro: Shadows die twice", "PS4", 60, true, Arrays.asList("JRPG", "Aventura")));
+
+        //Videojuegos de nintendo
+        listaVideojuegos.add(new Videojuego("The Legend of Zelda: Ocarina of Time", "N64", 40, false, Arrays.asList("Acción", "Aventura")));
+        listaVideojuegos.add(new Videojuego("Super Mario Galaxy 2", "Wii", 40, true, Arrays.asList("Plataformas", "Exploración")));
+        listaVideojuegos.add(new Videojuego("Pokémon Perla", "DS", 30, false, Arrays.asList("RPG", "Aventura")));
+
+        //Videojuegos de xbox
+        listaVideojuegos.add(new Videojuego("Halo Infinite", "XBOX Series X", 70, true, Arrays.asList("FPS", "Acción")));
+        listaVideojuegos.add(new Videojuego("Fable II", "XBOX 360", 30, false, Arrays.asList("RPG", "Aventura")));
+        listaVideojuegos.add(new Videojuego("Quantum Breake", "XBOX 360", 70, true, Arrays.asList("RPG", "Táctico")));
+
 
         //Pasar los videojuegos a JSON
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
